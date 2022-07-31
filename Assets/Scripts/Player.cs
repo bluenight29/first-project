0a14803b1411f8e9d69dd2eec9c7a27879c30d1c
찌롱 <jjj2901@naver.com>
2022-07-31 18:01:43 +0900

    플레이어 이동

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    float hAxis;
+    float vAxis;
+
+
+    Vector3 moveVec;
+
+    Animator anim;
+
+    void Start()
+    {
+
+
+    }
+
+    void Update()
+    {
+        hAxis = Input.GetAxisRaw("Horizontal");
+        vAxis = Input.GetAxisRaw("Vertical");
+
+        moveVec = new Vector3(hAxis, 0, vAxis).normalized;
+
+        transform.position += moveVec * speed * Time.deltaTime;
+
+    }
+}
